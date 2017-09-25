20df58a760b153792a53e83a89afbb80642dd88d
Gerard Ramos <gerard.ramosm89@gmail.com>
2017-09-25 12:05:27 -0700

    can add users

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,28 +1,67 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ListView, View, Text, FlatList } from 'react-native';
+import { connect } from 'react-redux';
+import { employeesFetch } from '../actions';
+import _ from 'lodash';
+import ListItem from './ListItem';
 
-export default class EmployeeList extends Component {
+class EmployeeList extends Component {
   constructor(props) {
     super(props);
-    this.employees = [
-      'Employee1',
-      'Employee2',
-      'Employee3',
-      'Employee4',
-      'Employee5',
-      'Employee6'
-    ]
+
   }
-  renderEmployees() {
-    return this.employees.map(employee => {
-      return <Text key={employee}>{employee}</Text>
+  componentWillMount() {
+    this.props.employeesFetch();
+    this.createDataSource(this.props);    
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.createDataSource(nextProps);
+    this.forceUpdate();
+  }
+
+  createDataSource({ employees }) {
+    console.log('... creating Data Source ... \n employees found is: ', employees);
+
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
     });
+    this.dataSource = ds.cloneWithRows(this.props.employees);
+    this.forceUpdate();
+  }
+  renderRow(employee) {
+    return <ListItem employee={employee} />
+  }
+  renderArray() {
+    return this.props.employees.map(employee => {
+      console.log('employee from renderArray is: ', employee);
+      return <ListItem key={employee.uid} employee={employee} />
+    });
+  }
+  renderFlatList() {
+    return(
+      <FlatList
+        data={this.props.employees}
+        renderItem={employee => <ListItem key={employee.uid} employee={employee} />}
+      />
+    );
   }
   render() {
     return(
       <View>
-        {this.renderEmployees()}
+        {this.renderFlatList()}
       </View>
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  const { employees } = state.employeeForm;
+  const mappedEmployees = _.map(employees, (val, uid) => {
+    return { ...val, uid }
+  })
+  return {
+    employees: mappedEmployees
+  }
+}
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -4,12 +4,11 @@ import { Actions } from 'react-native-router-flux';
 export default (state = INITIAL_STATE, action) => {
   switch(action.type) {
     case 'EMPLOYEE_UPDATE':
-      console.log('action.payload is: ', action.payload);
       return { ...state, [action.payload.prop]: action.payload.value }
     case 'EMPLOYEE_CREATE':
-      console.log('from reducer CREATE EMPLOYEE');
-      // Actions.employeeList({ type: 'reset' });
       return { ...state }
+    case 'EMPLOYEES_FETCH_SUCCESS':
+      return { ...state, employees: action.payload }
     default:
       return state;
   }
